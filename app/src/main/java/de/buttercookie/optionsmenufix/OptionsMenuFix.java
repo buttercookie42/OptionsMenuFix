c4cf5f60e2c92db059b356e07f23a1a76711690d
Jan Henning <jh+vsc-hg@buttercookie.de>
2023-12-28 21:01:51 +0100

    Force refresh of Decor View to work around Android bug
    In Android 14 and newer releases of Android 13 (somewhere between r31 and r38),
    a bug was introduced into Android which causes the options menu to open only
    on the first call of Activity.openOptionsMenu() and to ignore subsequent
    attempts to open the menu again.
    
    For more details, see https://issuetracker.google.com/issues/315761686.
    
    To work around the issue, we set refreshDecorView to true on closing the menu,
    in order to force a more complete re-initialisation the next time the menu is
    being opened.

--- app/src/main/java/de/buttercookie/optionsmenufix/OptionsMenuFix.java
+++ app/src/main/java/de/buttercookie/optionsmenufix/OptionsMenuFix.java
@@ -1,17 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 package de.buttercookie.optionsmenufix;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.setBooleanField;
 
 import androidx.annotation.Keep;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class OptionsMenuFix implements IXposedHookLoadPackage {
+    private static final String LOGTAG = "OptionsMenuFix";
+
     @Keep
     public OptionsMenuFix() {}
 
     @Keep
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-
+        findAndHookMethod("com.android.internal.policy.PhoneWindow", lpparam.classLoader,
+                "closePanel",
+                "com.android.internal.policy.PhoneWindow$PanelFeatureState", boolean.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        Object /* PanelFeatureState */ st = param.args[0];
+                        setBooleanField(st, "refreshDecorView", true);
+                    }
+                });
     }
 }
