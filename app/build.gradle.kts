86543124f626a4f894a4d8797a44d2a1713a6a5a
Jan Henning <jh+vsc-hg@buttercookie.de>
2023-12-28 22:48:10 +0100

    v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 33
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
